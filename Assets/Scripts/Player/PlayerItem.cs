232c541a39fc64756d3156be4406efa5af3d2a6f
Ina Joe <ij2049@nyu.edu>
2022-11-10 14:06:23 -0500

    Merge2

--- Assets/Scripts/Player/PlayerItem.cs
+++ Assets/Scripts/Player/PlayerItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,13 +6,99 @@ using UnityEngine.UI;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+<<<<<<< HEAD
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+=======
+>>>>>>> main
 
-public class PlayerItem : MonoBehaviour
+public class PlayerItem : MonoBehaviourPunCallbacks
 {
     [SerializeField] private TextMeshProUGUI playerName;
+<<<<<<< HEAD
+    [SerializeField] private Color highlightColor;
+    [SerializeField] private GameObject leftArrowButton;
+    [SerializeField] private GameObject rightArrowButton;
+    private Image backgroundImage;
+
+    private ExitGames.Client.Photon.Hashtable playerProperties = new ExitGames.Client.Photon.Hashtable();
+    [SerializeField] private Image playerAvatar;
+    [SerializeField] private Sprite[] avatars;
+
+    private Player player;
+    private void Start()
+    {
+        backgroundImage = GetComponent<Image>();
+
+        playerProperties["name"] = "Liam";
+        playerProperties["age"] = 15;
+        print(playerProperties["name"]);
+    }
 
     public void SetPlayerInfo(Player _player)
     {
         playerName.text = _player.NickName;
+        player = _player;
+        UpdatePlayerItem(player);
+    }
+
+    public void ApplyLocalChanges()
+    {
+        backgroundImage.color = highlightColor;
+        leftArrowButton.SetActive(true);
+        rightArrowButton.SetActive(true);
+    }
+
+    public void OnClickLeftArrow()
+    {
+        if ((int)playerProperties["playerAvatar"] == 0)
+        {
+            playerProperties["playerAvatar"] = avatars.Length - 1;
+        }
+        else
+        {
+            playerProperties["playerAvatar"] = (int)playerProperties["playerAvatar"] - 1;
+        }
+        PhotonNetwork.SetPlayerCustomProperties(playerProperties);
+    }
+
+    public void OnClickRightArrow()
+    {
+        if ((int)playerProperties["playerAvatar"] == avatars.Length - 1)
+        {
+            playerProperties["playerAvatar"] = 0;
+        }
+        else
+        {
+            playerProperties["playerAvatar"] = (int)playerProperties["playerAvatar"] + 1;
+        }
+
+        PhotonNetwork.SetPlayerCustomProperties(playerProperties);
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if (player == targetPlayer)
+        {
+            UpdatePlayerItem(targetPlayer);
+        }
+    }
+
+    private void UpdatePlayerItem(Player _player)
+    {
+        if (player.CustomProperties.ContainsKey("playerAvatar"))
+        {
+            playerAvatar.sprite = avatars[(int)player.CustomProperties["playerAvatar"]];
+            playerProperties["playerAvatar"] = (int) player.CustomProperties["playerAvatar"];
+        }
+        else
+        {
+            playerProperties["playerAvatar"] = 0;
+        }
+=======
+
+    public void SetPlayerInfo(Player _player)
+    {
+        playerName.text = _player.NickName;
+>>>>>>> main
     }
 }
